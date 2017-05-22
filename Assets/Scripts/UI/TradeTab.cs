aa1b3d7a9fa1ceaf52e9bb099cba283aead0773d
William Davies <wd6g14@soton.ac.uk>
2017-05-22 17:18:48 +0100

    Refactored TradeTab.cs

--- Assets/Scripts/UI/TradeTab.cs
+++ Assets/Scripts/UI/TradeTab.cs
@@ -3,14 +3,27 @@
 namespace UI {
     public class TradeTab : InteractionTab {
 
-        private TradeInteraction source;
+        #region Private Fields
 
-        public void Bind(TradeInteraction interaction) {
-            source = interaction;
+        private TradeInteraction _source;
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        internal void Bind(TradeInteraction interaction) {
+            _source = interaction;
         }
+
+        #endregion Public Methods
+
+        #region Internal Methods
 
         internal override string GetTitle() {
             return "Trade";
         }
+
+        #endregion Internal Methods
+
     }
 }
